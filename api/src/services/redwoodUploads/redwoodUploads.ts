704847f7851c3106fee49c80ac4fa913d5c5ac48
David Thyresson <dthyresson@gmail.com>
2024-10-12 10:43:38 -0400

    validate

--- api/src/services/redwoodUploads/redwoodUploads.ts
+++ api/src/services/redwoodUploads/redwoodUploads.ts
@@ -17,30 +17,46 @@ export const getRedwoodUploadToken: GetRedwoodUploadTokenResolver = async ({
 
   logger.debug({ operationName }, 'getRedwoodUploadToken')
 
+  let minFiles = 1
+  let maxFiles = 1
+  let expiresIn = 1 * 60 * 60 // 1 hour
+  let maxFileSize = 1 * 1024 * 1024 // 1MB
+
   let contentTypes
 
+  // based on the operation name, we can configure the content types, max file size, etc
   switch (operationName) {
     case 'Demo6':
       contentTypes = [...IMAGE_CONTENT_TYPES, ...DOCUMENT_CONTENT_TYPES]
+      maxFiles = 2
       break
     case 'Demo7':
       contentTypes = [...IMAGE_CONTENT_TYPES, ...DOCUMENT_CONTENT_TYPES]
+      maxFiles = 3
       break
     case 'Demo8':
+      contentTypes = IMAGE_CONTENT_TYPES
+      maxFiles = 3
+      break
+    case 'Demo9':
+      minFiles = 1
+      maxFiles = 1
+      expiresIn = 1 * 60 * 60 // 1 hour
+      maxFileSize = 10 * 1024 * 1024 // 10MB
       contentTypes = IMAGE_CONTENT_TYPES
       break
     default:
       contentTypes = IMAGE_CONTENT_TYPES
   }
 
-  // Note: based on the operation name, we could configure the content types, max file size, etc
+  // Note: based on the operation name,we could configure the content types, max file size, etc
 
   const payload: UploadTokenPayload = {
     operationName,
-    minFiles: 1,
-    maxFiles: 3,
-    expiresIn: 1 * 60 * 60, // 1 hour
-    maxFileSize: 1 * 1024 * 1024, // 1MB
+    minFiles,
+    maxFiles,
+    expiresIn,
+    maxFileSize,
     contentTypes,
   }
 
